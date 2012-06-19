7f951546aa8fbb0520e18ae8bb13ad6ccd6df64e
liwei <hi@liwei.me>
2012-06-19 22:36:37 +0800

    0.0.3

--- init.rb
+++ init.rb
@@ -1,13 +1,14 @@
 # liwei created at 2012-2-20
 # 0.0.1 basic
 # 0.0.2 add jiathis to asset's view
+# 0.0.3 chang some words
 require 'redmine'
 
 Redmine::Plugin.register :redmine_assets do
   name 'Redmine Assets plugin'
   author 'Riquel Li'
   description 'This is a plugin for Redmine to manage your assets.'
-  version '0.0.2'
+  version '0.0.3'
   url 'http://railser.cn'
   author_url 'http://railser.cn/me'
 
@@ -19,7 +20,7 @@ Redmine::Plugin.register :redmine_assets do
     permission :view_assets, {:assets => [:index, :show, :all]}
   end
 
-  menu :project_menu, :assets, {:controller => "assets", :action => "index"}, :caption => "Assets", :param => :project_id
+  menu :project_menu, :assets, {:controller => "assets", :action => "index"}, :caption => "Extensions", :param => :project_id
 
 end
 
